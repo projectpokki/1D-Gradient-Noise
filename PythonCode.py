b2d2c01863d425d1b9129c51852703952eebcfc7
projectpokki <158499943+projectpokki@users.noreply.github.com>
2024-05-19 22:33:25 +0800

    Update PythonCode.py

--- PythonCode.py
+++ PythonCode.py
@@ -1,7 +1,7 @@
 from math import floor
 
 
-def pokki_noise_compact(inputNum: float, seed: list, k: float):
+def pokki_noise(inputNum: float, seed: list, k: float):
   inputFrac, inputInt = inputNum % 1, floor(inputNum)
   a, b, c = 0, 0, 0
   positiveWeight, negativeWeight = inputFrac ** k, (1 - inputFrac) ** k
